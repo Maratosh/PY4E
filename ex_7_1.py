4990436925c1537e1b349787a14fb45c250275b3
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-05-17 13:04:24 +0300

    Update ex_7_1.py

--- ex_7_1.py
+++ ex_7_1.py
@@ -9,3 +9,32 @@ for line in fhand:
     line = line.upper()
     line = line.rstrip()
     print(line)
+############################################
+#!/usr/bin/python3
+
+''' Exercise 2: Write a program to prompt for a
+file name, and then read through the file and look for
+lines of the form: X-DSPAM-Confidence: 0.8475
+When you encounter a line that starts with "X-DSPAM-Confidence:"
+pull apart the line to extract the floating-point number on the line.
+Count these lines and then compute the total of the spam confidence
+values from these lines. When you reach the end of the file, print out
+the average spam confidence.  Average spam confidence: 0.750718518519 '''
+
+fname = input('Enter the filename: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened: ', fname)
+    exit()
+    
+count = 0
+snum = 0
+for line in fhand:
+    line = line.rstrip()
+    if line.find('X-DSPAM-Confidence:') == -1:
+        continue
+    print(line, float(line[19:]))
+    snum = snum + float(line[19:])
+    count += 1
+print(snum / count)
