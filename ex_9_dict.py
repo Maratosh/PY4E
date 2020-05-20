57ab3aa17f22377bc757fd1f5dccfebe81250b08
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-05-20 20:49:42 +0300

    Update ex_9_dict.py

--- ex_9_dict.py
+++ ex_9_dict.py
@@ -14,3 +14,14 @@ for line in open('words.txt'):
         item += 1
 print(dict1)
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Same but counts words
+dict1 = {}
+for line in open('words.txt'):
+    line = line.rstrip()
+    line = line.split()
+    for word in line:
+        if word not in dict1: 
+            dict1[word] = 1
+        else:
+            dict1[word] = dict1[word] + 1
+print(dict1)
