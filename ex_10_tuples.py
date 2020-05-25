0a42c4b522ac5746e28ddfa4e838381806c10f5e
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-05-25 21:08:40 +0300

    Update ex_10_tuples.py

--- ex_10_tuples.py
+++ ex_10_tuples.py
@@ -11,4 +11,19 @@ lst = [(len(word), word) for word in txt.split()]
 lst.sort(reverse=True)
 for l, w in lst:
     print(w, l)
+#!Dictionaries and tuples
 
+
+d = {'a':10, 'b':1, 'c':22}
+lst = [(v, k) for k, v in list(d.items())]
+lst.sort()
+print(lst)                          #[(1, 'b'), (10, 'a'), (22, 'c')]
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+d = {'a':10, 'b':1, 'c':22}
+lst = [(v, k) for k, v in list(d.items())]
+print(d)
+d1 = {}             #{'a': 10, 'b': 1, 'c': 22}
+for v, k in lst:
+    d1[v] = k
+print(d1)           #{10: 'a', 1: 'b', 22: 'c'}
