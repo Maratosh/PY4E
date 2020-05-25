c894527fcbb9ceb93c145d7ecbbf339b8688a238
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-05-25 16:49:14 +0300

    Update ex_10_tuples.py

--- ex_10_tuples.py
+++ ex_10_tuples.py
@@ -1,1 +1,16 @@
+''' DSU
+#!Decorate
+a sequence by building a list of tuples with one or more sort keys preceding the elements from the sequence,
+#!Sort
+the list of tuples using the Python built-in sort, and
+#!Undecorate
+by extracting the sorted elements of the sequence'''
+
+txt = 'but soft what light in yonder window breaks'
+words = txt.split()
+#print(words)
+t = [(len(word), word) for word in words]
+t.sort(reverse=True)
+for l, w in t:
+    print(w, l)
 
