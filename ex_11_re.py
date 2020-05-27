295e30ab3808bc58d88adcb0a3dcb12bf496f86d
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-05-27 11:19:43 +0300

    Update ex_11_re.py

--- ex_11_re.py
+++ ex_11_re.py
@@ -25,12 +25,47 @@ import re
 fhand = open('mbox-short.txt')
 for line in fhand:
     line = line.rstrip()
-    if re.search('From', line):     #line.find('From') >=0:
+    if re.search('F..m', line):     #line.find('From') >=0:
         print(line)
 #####################################
-import re
+import re                           
 fhand = open('mbox-short.txt')
 for line in fhand:
     line = line.rstrip()
-    if re.search('^From', line):  #line.startswith('From'):
+    if re.search('^F..m', line):  #line.startswith('From'):
         print(line)
+########################################
+import re
+fhand = open('mbox-short.txt')        # The search string ^From:.+@ will successfully match lines that start with “From:”, 
+for line in fhand:                    # followed by one or more characters (.+), followed by an at-sign.      
+    line = line.rstrip()              # So this will match the following line: From: stephen.marquard@uct.ac.za
+    if re.search('^F..m:.+@', line): 
+        print(line)                   
+        
+        
+#############################################        
+# Extracting data using regular expressions        
+# Searching for lines that have an at-sign between characters
+import re                             # The findall() method searches the string in the second argument 
+fhand = open('mbox.txt')              # and returns a list of all of the strings that look like email addresses. 
+for line in fhand:                    # We are using a two-character sequence that matches a non-whitespace character (\S).
+    line = line.rstrip()
+    x = re.findall('\S+@\S+', line)
+    if len(x) > 0: 
+        print(x)
+        
+####################################
+# Search for lines that have an at sign between characters
+# The characters must be a letter or number
+import re                                               # we are looking for substrings that start with a single lowercase letter, 
+fhand = open('mbox.txt')                                # uppercase letter, or number “[a-zA-Z0-9]”, followed by zero or more 
+for line in fhand:                                      # non-blank characters (\S*), followed by an at-sign, followed by zero or 
+    line = line.rstrip()                                # more non-blank characters (\S*), followed by an uppercase or 
+    x = re.findall('[a-zA-Z0-9]\S*@\S*[a-zA-Z]', line)  # lowercase letter [a-zA-Z]
+    if len(x) > 0:
+        print(x)
+
+
+
+
+
