71ef48657fae2f835b8bf232d3ad30ca02a0cdab
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-05-28 22:44:17 +0300

    Update ex_11_re.py

--- ex_11_re.py
+++ ex_11_re.py
@@ -134,6 +134,35 @@ fhand = open('mbox.txt')
 for line in fhand:
     line = line.rstrip()
     if re.search(rex, line):
-        print(line)
+        i += 1
+print('This file has {} lines that matches {}'.format(i, rex))
+#########################
+#!/usr/bin/python3
+'''Exercise 2: Write a program to look for lines of the form:
+New Revision: 39772
+Extract the number from each of the lines using a regular expression
+and the findall() method. Compute the average of the numbers and print
+out the average as an integer.'''
+#!/usp/bin/python3
+import re
+#rex = input('Enter a regular expression: ')
+i = 0
+lst = []
+fname = input('Enter file: ')
+try:
+    fhand = open(fname)
+except:
+    print('The file cannot be opened!', fname)
+    exit()
+for line in fhand:
+    line = line.rstrip()
+    x = re.findall('New Revision: ([0-9]+)', line)
+    if len(x) > 0:
+        #print(x)
+        for i in x:
+            #print(i)
+            i = int(i)
+            lst.append(i)
+print(int(sum(lst)/len(lst)))
     
 
