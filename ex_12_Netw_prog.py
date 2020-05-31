92d6b62c2e1673a803767e714234d859ebfb4459
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-05-31 22:02:25 +0300

    Update ex_12_Netw_prog.py

--- ex_12_Netw_prog.py
+++ ex_12_Netw_prog.py
@@ -65,5 +65,36 @@ for line in fhand:
         count[word] = count.get(word, 0) + 1
 
 print(count)
-###############
+## Reading binary files using urllib  #####
 
+'''This program reads all of the data in at once across the network and stores it in the variable img in the main
+memory of your computer, then opens the file cover.jpg and writes the data out to your disk. The wb argument for open() 
+opens a binary file for writing only. This program will work if the size of the file is less than the size of the memory
+of your computer.
+However if this is a large audio or video file, this program may crash or at least run extremely slowly when your computer
+runs out of memory. In order to avoid running out of memory, we retrieve the data in blocks (or buffers) and then write each
+block to your disk before retrieving the next block. This way the program can read any size file without using up all of the
+memory you have in your computer.'''
+import urllib.request, urllib.parse, urllib.error
+
+img = urllib.request.urlopen('https://upload.wikimedia.org/wikipedia/commons/7/7d/Crew_Dragon_at_the_ISS_for_Demo_Mission_1_%28cropped%29.jpg').read()
+fhand = open('dragon.jpg', 'wb')
+fhand.write(img)
+fhand.close()
+#####
+import urllib.request, urllib.parse, urllib.error
+
+img = urllib.request.urlopen('http://data.pr4e.org/cover3.jpg')
+fhand = open('cover3.jpg', 'wb')
+size = 0
+while True:
+    info = img.read(100000)
+    if len(info) < 1: break
+    size = size + len(info)
+    fhand.write(info)
+
+print(size, 'characters copied.')
+fhand.close()
+
+# Code: http://www.py4e.com/code3/curl2.py
+#################################
