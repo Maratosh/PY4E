3a126eca9aed320a1d1cc9280872c1960874681d
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-06-01 10:54:48 +0300

    Update ex_12_Netw_prog.py

--- ex_12_Netw_prog.py
+++ ex_12_Netw_prog.py
@@ -143,3 +143,92 @@ Information on installing BeautifulSoup with the Python Package Index tool pip i
 https://packaging.python.org/tutorials/installing-packages/
 We will use urllib to read the page and then use BeautifulSoup to extract the href attributes from the anchor (a) tags.'''
 
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
+# https://wiki.python.org/moin/BeginnersGuide
+# Code: http://www.py4e.com/code3/urllinks.py
+'''The program prompts for a web address, then opens the web page, reads the data and passes the data to the BeautifulSoup
+parser, and then retrieves all of the anchor tags and prints out the href attribute for each tag'''
+
+'''This list is much longer because some HTML anchor tags are relative paths (e.g., tutorial/index.html) or in-page references 
+(e.g., ‘#’) that do not include “http://” or “https://”, which was a requirement in our regular expression.
+
+You can use also BeautifulSoup to pull out various parts of each tag: '''
+
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    # Look at the parts of a tag
+    print('TAG:', tag)
+    print('URL:', tag.get('href', None))
+    print('Contents:', tag.contents[0])
+    print('Attrs:', tag.attrs)
+
+# Code: http://www.py4e.com/code3/urllink2.py
+'''html.parser is the HTML parser included in the standard Python 3 library. Information on other HTML parsers is available at:
+http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
+These examples only begin to show the power of BeautifulSoup when it comes to parsing HTML.'''
+
+''' Bonus section for Unix / Linux users
+If you have a Linux, Unix, or Macintosh computer, you probably have commands built in to your operating system that retrieves both plain text and binary files using the HTTP or File Transfer (FTP) protocols. One of these commands is curl:
+
+$ curl -O http://www.py4e.com/cover.jpg
+The command curl is short for “copy URL” and so the two examples listed earlier to retrieve binary files with urllib are cleverly named curl1.py and curl2.py on www.py4e.com/code3 as they implement similar functionality to the curl command. There is also a curl3.py sample program that does this task a little more effectively, in case you actually want to use this pattern in a program you are writing.
+
+A second command that functions very similarly is wget:
+
+$ wget http://www.py4e.com/cover.jpg
+Both of these commands make retrieving webpages and remote files a simple task.'''
+''' Glossary
+BeautifulSoup
+A Python library for parsing HTML documents and extracting data from HTML documents that compensates for most of the imperfections
+in the HTML that browsers generally ignore. You can download the BeautifulSoup code from www.crummy.com.
+port
+A number that generally indicates which application you are contacting when you make a socket connection to a server. As an 
+example, web traffic usually uses port 80 while email traffic uses port 25.
+scrape
+When a program pretends to be a web browser and retrieves a web page, then looks at the web page content. Often programs are 
+following the links in one page to find the next page so they can traverse a network of pages or a social network.
+socket
+A network connection between two applications where the applications can send and receive data in either direction.
+spider
+The act of a web search engine retrieving a page and then all the pages linked from a page and so on until they have nearly
+all of the pages on the Internet which they use to build their search index.'''
+
+
