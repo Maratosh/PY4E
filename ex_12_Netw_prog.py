b9f1363e3b6925517bb0fa9e2505b63b19e6e5f5
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-06-01 10:22:37 +0300

    Update ex_12_Netw_prog.py

--- ex_12_Netw_prog.py
+++ ex_12_Netw_prog.py
@@ -98,3 +98,48 @@ fhand.close()
 
 # Code: http://www.py4e.com/code3/curl2.py
 #################################
+# Parsing HTML and scraping the web
+'''One of the common uses of the urllib capability in Python is to scrape the web. Web scraping is when we write 
+a program that pretends to be a web browser and retrieves pages, then examines the data in those pages looking for patterns.'''
+### Parsing HTML using regular expressions
+'''Our regular expression looks for strings that start with “href="http://” or “href="https://”, followed by one or more
+characters (.+?), followed by another double quote. The question mark behind the [s]? indicates to search for the string
+“http” followed by zero or one “s”.
+The question mark added to the .+? indicates that the match is to be done in a “non-greedy” fashion instead of a “greedy” 
+fashion. A non-greedy match tries to find the smallest possible matching string and a greedy match tries to find the largest
+possible matching string.
+We add parentheses to our regular expression to indicate which part of our matched string we would like to extract, and produce
+the following program:'''
+
+# Search fo link values within URL input
+import urllib.request, urllib.parse, urllib.error
+import re
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verigy_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+links = re.findall(b'href="(http[s]?://.*?)"', html)
+for link in links:
+    print(link.decode())
+
+'''The ssl library allows this program to access web sites that strictly enforce HTTPS. The read method returns HTML
+source code as a bytes object instead of returning an HTTPResponse object. The findall regular expression method will give
+us a list of all of the strings that match our regular expression, returning only the link text between the double quotes.'''
+
+##Parsing HTML using BeautifulSoup
+'''Even though HTML looks like XML1 and some pages are carefully constructed to be XML, most HTML is generally broken in
+ways that cause an XML parser to reject the entire page of HTML as improperly formed.
+There are a number of Python libraries which can help you parse HTML and extract data from the pages. Each of the libraries
+has its strengths and weaknesses and you can pick one based on your needs.
+As an example, we will simply parse some HTML input and extract links using the BeautifulSoup library. BeautifulSoup tolerates 
+highly flawed HTML and still lets you easily extract the data you need. You can download and install the BeautifulSoup code from:
+https://pypi.python.org/pypi/beautifulsoup4
+Information on installing BeautifulSoup with the Python Package Index tool pip is available at:
+https://packaging.python.org/tutorials/installing-packages/
+We will use urllib to read the page and then use BeautifulSoup to extract the href attributes from the anchor (a) tags.'''
+
