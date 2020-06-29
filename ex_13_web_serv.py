89ca24c4c0d690c8387ac9a30f13d4fe58032deb
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-06-29 10:18:01 +0300

    Update ex_13_web_serv.py

--- ex_13_web_serv.py
+++ ex_13_web_serv.py
@@ -14,3 +14,7 @@ data = '''
 tree = ET.fromstring(data)
 print('Name:', tree.find('name').text)
 print('Attr:', tree.find('email').get('hide'))
+
+''' Calling fromstring converts the string representation of the XML into a “tree” of XML elements. When the XML is in a tree,
+we have a series of methods we can call to extract portions of data from the XML string. The find function searches through the XML tree 
+and retrieves the element that matches the specified tag '''
