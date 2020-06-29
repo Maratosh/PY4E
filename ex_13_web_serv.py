7d36d19713bde081fe6bd05600f39777accd99de
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-06-29 10:19:44 +0300

    Update ex_13_web_serv.py

--- ex_13_web_serv.py
+++ ex_13_web_serv.py
@@ -18,3 +18,7 @@ print('Attr:', tree.find('email').get('hide'))
 ''' Calling fromstring converts the string representation of the XML into a “tree” of XML elements. When the XML is in a tree,
 we have a series of methods we can call to extract portions of data from the XML string. The find function searches through the XML tree 
 and retrieves the element that matches the specified tag '''
+'''Using an XML parser such as ElementTree has the advantage that while the XML in this example is quite simple, it turns out there are many rules 
+regarding valid XML, and using ElementTree allows us to extract data from XML without worrying about the rules of XML syntax.'''
+
+# Looping through nodes
