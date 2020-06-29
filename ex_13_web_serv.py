8227d0370f68b869c7b4a8710e1bae1db3f6c690
Maratosh <49524488+Maratosh@users.noreply.github.com>
2020-06-29 10:40:24 +0300

    Update ex_13_web_serv.py

--- ex_13_web_serv.py
+++ ex_13_web_serv.py
@@ -22,3 +22,32 @@ and retrieves the element that matches the specified tag '''
 regarding valid XML, and using ElementTree allows us to extract data from XML without worrying about the rules of XML syntax.'''
 
 # Looping through nodes
+''' Often the XML has multiple nodes and we need to write a loop to process all of the nodes. In the following program, we loop through all of the user nodes:'''
+import xml.etree.ElementTree as ET
+
+input = '''
+<stuff>
+    <users>
+        <user x="2">
+            <id>001</id>
+            <name>Chuck</name>
+        </user>
+        <user x="7">
+            <id>009</id>
+            <name>Brent</name>
+        </user>
+    </users>
+</stuff> '''
+
+stuff = ET.fromstring(input)
+lst = stuff.findall('users/user')
+print('User count:', len(lst))
+
+for item in lst:
+    print('Name', item.find('name').text)
+    print('Id', item.find('id').text)
+    print('Attribute', item.get('x'))
+'''The findall method retrieves a Python list of subtrees that represent the user structures in the XML tree. Then we can write a for loop that looks at 
+each of the user nodes, and prints the name and id text elements as well as the x attribute from the user node.'''
+'''It is important to include all parent level elements in the findall statement except for the top level element (e.g., users/user). Otherwise, Python will not find 
+any desired nodes.'''
